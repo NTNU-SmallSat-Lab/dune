5e9f5762b966de6988b35d5630c074b044040df9
Joao Fortuna <fortuna.araujo@gmail.com>
2017-03-03 13:42:53 +0100

    Vision/UEye: Previously known as UI2210MGL. Removed dependence from OpenCV. Cleaned unused code.

---
+++ CaptureUeye.hpp
@@ -0,0 +1,369 @@
+//***************************************************************************
+// Copyright 2013-2017 Norwegian University of Science and Technology (NTNU)*
+// Centre for Autonomous Marine Operations and Systems (AMOS)               *
+// Department of Engineering Cybernetics (ITK)                              *
+//***************************************************************************
+// This file is part of DUNE: Unified Navigation Environment.               *
+//                                                                          *
+// Commercial Licence Usage                                                 *
+// Licencees holding valid commercial DUNE licences may use this file in    *
+// accordance with the commercial licence agreement provided with the       *
+// Software or, alternatively, in accordance with the terms contained in a  *
+// written agreement between you and Universidade do Porto. For licensing   *
+// terms, conditions, and further information contact lsts@fe.up.pt.        *
+//                                                                          *
+// European Union Public Licence - EUPL v.1.1 Usage                         *
+// Alternatively, this file may be used under the terms of the EUPL,        *
+// Version 1.1 only (the "Licence"), appearing in the file LICENCE.md       *
+// included in the packaging of this file. You may not use this work        *
+// except in compliance with the Licence. Unless required by applicable     *
+// law or agreed to in writing, software distributed under the Licence is   *
+// distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF     *
+// ANY KIND, either express or implied. See the Licence for the specific    *
+// language governing permissions and limitations at                        *
+// http://ec.europa.eu/idabc/eupl.html.                                     *
+//***************************************************************************
+// Author: João Fortuna                                                     *
+//***************************************************************************
+
+#ifndef VISION_UEYE_CAPUEYE_HPP_INCLUDED_
+#define VISION_UEYE_CAPUEYE_HPP_INCLUDED_
+
+// ISO C++ 98 headers.
+#include <queue>
+
+// DUNE headers.
+#include <DUNE/DUNE.hpp>
+
+// Vendor headers.
+#include <ueye.h>
+
+using DUNE_NAMESPACES;
+
+namespace Vision
+{
+  //! Device driver for uEye Cameras.
+  //!
+  //! @author João Fortuna
+  namespace UEye
+  {
+    //! Area of Interest
+    struct AOI
+    {
+      //! Upper left corner coordinates
+      unsigned x, y;
+      //! Size
+      unsigned width, height;
+    };
+
+    //! Frame
+    struct Frame
+    {
+      //! Data pointer
+      char* data;
+      //! Sequence number
+      unsigned long long seqNum;
+      //! Timestamp
+      double timestamp;
+      //! ID
+      unsigned int id;
+      //! Gain in percent of original value
+      int gainFactor;
+    };
+
+    class CaptureUeye: public Thread
+    {
+    public:
+      //! Constructor.
+      //! @param[in] task parent task.
+      //! @param[in] buffer_capacity packet buffer capacity.
+      CaptureUeye(DUNE::Tasks::Task* task, AOI aoi, HIDS cam = 1, double fps = 30.0):
+        m_task(task),
+        m_cam(cam),
+        m_aoi(aoi),
+        m_fps(fps),
+        m_read(0),
+        m_write(0),
+        m_lastTS(0)
+      {
+        m_imgMems = new char*[c_buf_len];
+        m_imgMemIds = new int[c_buf_len];
+        m_frames = new Frame[c_buf_len];
+
+        initializeCam();
+      }
+
+      //! Destructor.
+      ~CaptureUeye(void)
+      {
+        for (unsigned i = 0; i < c_buf_len; i++)
+        {
+          int tmp = is_FreeImageMem(m_cam, m_imgMems[i], m_imgMemIds[i]);
+
+          if (tmp)
+          {
+            m_task->err("Trying to free image buffer %d. Error %d", i, tmp);
+            break;
+          }
+        }
+        Memory::clear(m_imgMems);
+        Memory::clear(m_imgMemIds);
+        Memory::clear(m_frames);
+
+        is_ExitCamera(m_cam);
+      }
+
+      void
+      setAOI(AOI aoi)
+      {
+        IS_RECT rectAOI;
+
+        rectAOI.s32X      = aoi.x;
+        rectAOI.s32Y      = aoi.y;
+        rectAOI.s32Width  = aoi.width;
+        rectAOI.s32Height = aoi.height;
+
+        int tmp = is_AOI(m_cam, IS_AOI_IMAGE_SET_AOI, (void*)&rectAOI, sizeof(rectAOI));
+        if (tmp)
+          m_task->err("AOI unsuccessful. Error %d", tmp);
+
+        // Allocate memory
+
+        // Will hold the picture ID of an image. I think this is an image's
+        // index within a memory area allocated to images
+        int pid;
+
+        for (unsigned i = 0; i < c_buf_len; i++)
+        {
+          tmp = is_AllocImageMem(m_cam, m_aoi.width, m_aoi.height, 8, &m_ppcImgMem, &pid);
+
+          // Store pointer and picture ID for this memory
+          m_imgMems[i] = m_ppcImgMem;
+          m_imgMemIds[i] = pid;
+
+          if (tmp)
+          {
+            m_task->err("Allocate image buffer %d unsuccessful. Error %d", i, tmp);
+            break;
+          }
+        }
+      }
+
+      void
+      setFPS(double fps)
+      {
+        // Set target FPS
+        double newFPS, fpsWish = fps;
+        is_SetFrameRate(m_cam, fpsWish, &newFPS);
+        m_task->debug("Requested %.2f FPS, actual FPS is now %.2f", fpsWish, newFPS);
+      }
+
+      void
+      setExposure(float exp)
+      {
+        // Set target Exposure time
+        double newExp = exp;
+        is_Exposure (m_cam, IS_EXPOSURE_CMD_SET_EXPOSURE, (void*) &newExp, 8);
+        m_task->debug("Requested Exposure time %.2fms, actual is now %.2fms", exp, newExp);
+      }
+
+      void
+      setGain(bool autogain, int gain)
+      {
+        //Enable or disable auto gain control:
+        double param = autogain ? 1 : 0;
+        int ret = is_SetAutoParameter (m_cam, IS_SET_ENABLE_AUTO_GAIN, &param, 0);
+        if (ret == IS_SUCCESS)
+          m_task->debug("%s Auto Gain.", autogain ? "Enabled" : "Disabled");
+        else
+          m_task->err("%s Auto Gain unsuccessful. Error %d", autogain ? "Enable" : "Disable", ret);
+
+        //Set gain if not auto:
+        ret = is_SetHardwareGain (m_cam, gain, IS_IGNORE_PARAMETER, IS_IGNORE_PARAMETER, IS_IGNORE_PARAMETER);
+        if (ret == IS_SUCCESS)
+          m_task->debug("Set Gain to %d.", gain);
+        else
+          m_task->err("SetHardwareGain unsuccessful. Error %d", ret);
+      }
+
+      Frame*
+      readFrame(void)
+      {
+        if ((m_read % c_buf_len) == (m_write % c_buf_len))
+          return NULL;
+
+        return &m_frames[m_read++ % c_buf_len];
+      }
+
+      void
+      stopCapture(void)
+      {
+        int ret = is_StopLiveVideo(m_cam, IS_FORCE_VIDEO_STOP);
+        if (ret == IS_SUCCESS)
+          m_task->inf("Image capture stopped");
+        else
+          m_task->err("StopLiveVideo unsuccessful. Error %d", ret);
+      }
+
+      int
+      queryGainFactor(int gain)
+      {
+          return is_SetHWGainFactor(m_cam, IS_INQUIRE_MASTER_GAIN_FACTOR, gain);
+      }
+
+    private:
+      //! Parent task.
+      DUNE::Tasks::Task* m_task;
+      //! Array of captured frames.
+      Frame* m_frames;
+      //! Camera handle.
+      HIDS m_cam;
+      //! Will contain the address of memory allocated for images.
+      char* m_ppcImgMem;
+      //! Area of Interest.
+      AOI m_aoi;
+      //! Frames per Second.
+      double m_fps;
+      //! Will contain pointers to all image allocated memory areas
+      char **m_imgMems;
+      //! Will contain picture IDs for the above array
+      int *m_imgMemIds;
+      //! Buffer Size
+      static const unsigned c_buf_len = 256;
+      //! Reader/Writer positions.
+      unsigned m_read, m_write;
+      //! Lookup table for gain factors
+      int m_gains[101];
+      //! Last timestamp
+      unsigned long long m_lastTS;
+
+      void
+      initializeCam(void)
+      {
+        int tmp;
+        // Will contain information about the sensor
+        SENSORINFO sensorInfo;
+
+        // Starts the driver and establishes the connection to the camera
+        tmp = is_InitCamera(&m_cam, NULL);
+
+        // Check if camera initialization was successfull.
+        if (tmp != IS_SUCCESS)
+          m_task->err("Camera initialization unsuccessful. Error %d", tmp);
+
+        // Enables automatic closing of the camera handle after a camera has been removed on-the-fly
+        tmp = is_EnableAutoExit(m_cam, IS_ENABLE_AUTO_EXIT);
+
+        // Check if EnableAutoExit was successfull.
+        if (tmp != IS_SUCCESS)
+          m_task->err("EnableAutoExit unsuccessful. Error %d", tmp);
+
+        // Get information about the sensor type used in the camera
+        tmp = is_GetSensorInfo(m_cam, &sensorInfo);
+        if (tmp == IS_SUCCESS)
+        {
+          m_task->debug("Sensor name: %s", sensorInfo.strSensorName);
+          m_task->debug("Resolution: %d x %d", sensorInfo.nMaxWidth, sensorInfo.nMaxHeight);
+        }
+        else
+          m_task->err("GetSensorInfo unsuccessful. Error %d", tmp);
+
+        // Set color mode.
+        is_SetColorMode(m_cam, IS_CM_SENSOR_RAW8);
+
+        // Set the pixel clock. This is capped at 30. Higher pixel clock
+        // will enable higher FPS.
+        UINT nPixelClockDefault = 20;
+        tmp = is_PixelClock(m_cam, IS_PIXELCLOCK_CMD_SET,
+            (void*)&nPixelClockDefault,
+            sizeof(nPixelClockDefault));
+        if (tmp != IS_SUCCESS)
+          m_task->err("PixelClock unsuccessful. Error %d", tmp);
+
+        setAOI(m_aoi);
+        setFPS(m_fps);
+        is_SetDisplayMode(m_cam, IS_SET_DM_DIB);
+        is_SetImageMem(m_cam, m_imgMems[0], m_imgMemIds[0]);
+
+        for (int i = 0; i <= 100; i++)
+        {
+          m_gains[i] = queryGainFactor(i);
+        }
+
+        // Enable the FRAME event. Triggers when a frame is ready in memory.
+        tmp = is_EnableEvent(m_cam, IS_SET_EVENT_FRAME);
+        if (tmp)
+          m_task->debug("Enabled FRAME event with status %d", tmp);
+      }
+
+      void
+      run(void)
+      {
+        int stat = is_CaptureVideo(m_cam, IS_DONT_WAIT);
+        if (stat)
+          m_task->err("Failed to activate image capture with error %d", stat);
+        else
+          m_task->inf("Image capture started");
+
+        // Small delay to let camera start
+        Time::Delay::wait(1.0);
+
+        while (!isStopping() && !stat)
+        {
+          stat = is_SetImageMem(m_cam, m_imgMems[m_write % c_buf_len], m_imgMemIds[m_write % c_buf_len]);
+          if (stat)
+          {
+            m_task->err("SetImageMem Error: %d", stat);
+
+            Time::Delay::wait(0.1);
+            continue;
+          }
+
+          stat = is_WaitEvent(m_cam, IS_SET_EVENT_FRAME, 1000);
+          if (stat)
+          {
+            if (stat != IS_TIMED_OUT)
+              m_task->err("WaitEvent error: %d", stat);
+          }
+          else
+          {
+            m_task->spew("Captured! %d", m_write);
+
+            Frame frame;
+
+            UEYEIMAGEINFO imageInfo;
+            int nRet = is_GetImageInfo(m_cam, m_imgMemIds[m_write % c_buf_len], &imageInfo, sizeof(imageInfo));
+            if (nRet == IS_SUCCESS)
+            {
+              // Get internal timestamp of image capture (tick count of the camera in 0.1 μs steps)
+              m_task->spew("Time diff: %llu", imageInfo.u64TimestampDevice - m_lastTS);
+              m_lastTS = imageInfo.u64TimestampDevice;
+
+              // Get frame number
+              frame.seqNum = imageInfo.u64FrameNumber;
+              m_task->spew("Frame: %llu", frame.seqNum);
+            }
+
+            int gain = is_SetHardwareGain(m_cam, IS_GET_MASTER_GAIN, IS_IGNORE_PARAMETER, IS_IGNORE_PARAMETER, IS_IGNORE_PARAMETER);
+            is_GetImageMem(m_cam, (void**)(&m_ppcImgMem));
+
+            frame.data = m_imgMems[m_write % c_buf_len];
+            frame.id = m_imgMemIds[m_write % c_buf_len];
+            frame.timestamp = Clock::getSinceEpoch();
+            frame.gainFactor = m_gains[gain];
+
+            m_frames[m_write++ % c_buf_len] = frame;
+
+            if (m_write == m_read)
+            {
+              m_task->err("Buffer overrun!");
+              Time::Delay::wait(0.1);
+            }
+          }
+        }
+      }
+    };
+  }
+}
+
+#endif
---
+++ Task.cpp
@@ -0,0 +1,342 @@
+//***************************************************************************
+// Copyright 2013-2017 Norwegian University of Science and Technology (NTNU)*
+// Centre for Autonomous Marine Operations and Systems (AMOS)               *
+// Department of Engineering Cybernetics (ITK)                              *
+//***************************************************************************
+// This file is part of DUNE: Unified Navigation Environment.               *
+//                                                                          *
+// Commercial Licence Usage                                                 *
+// Licencees holding valid commercial DUNE licences may use this file in    *
+// accordance with the commercial licence agreement provided with the       *
+// Software or, alternatively, in accordance with the terms contained in a  *
+// written agreement between you and Universidade do Porto. For licensing   *
+// terms, conditions, and further information contact lsts@fe.up.pt.        *
+//                                                                          *
+// European Union Public Licence - EUPL v.1.1 Usage                         *
+// Alternatively, this file may be used under the terms of the EUPL,        *
+// Version 1.1 only (the "Licence"), appearing in the file LICENCE.md       *
+// included in the packaging of this file. You may not use this work        *
+// except in compliance with the Licence. Unless required by applicable     *
+// law or agreed to in writing, software distributed under the Licence is   *
+// distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF     *
+// ANY KIND, either express or implied. See the Licence for the specific    *
+// language governing permissions and limitations at                        *
+// http://ec.europa.eu/idabc/eupl.html.                                     *
+//***************************************************************************
+// Author: João Fortuna                                                     *
+//***************************************************************************
+
+// ISO C++ 98 headers.
+#include <queue>
+#include <string>
+
+// DUNE headers.
+#include <DUNE/DUNE.hpp>
+
+// Local headers.
+#include <Vision/UEye/CaptureUeye.hpp>
+
+using DUNE_NAMESPACES;
+
+namespace Vision
+{
+  //! Device driver for uEye Cameras.
+  //!
+  //! @author João Fortuna
+  namespace UEye
+  {
+    //! %Task arguments.
+    struct Arguments
+    {
+      //! Camera ID
+      unsigned cam_id;
+      //! Frames Per Second.
+      unsigned fps;
+      //! Path to log directory
+      std::string log_dir;
+      //! Area of Interest specification
+      AOI aoi;
+      //! Auto Gain
+      bool auto_gain;
+      //! Gain
+      int gain;
+      //! Exposure time
+      float exposure;
+      //! Calibration mode
+      bool calib_mode;
+      //! Calibration delta
+      float calib_delta;
+    };
+
+    //! Device driver task.
+    struct Task: public DUNE::Tasks::Task
+    {
+      //! %Frame width. Unclear if 640 or 960
+      static const unsigned c_width = 640;
+      //! %Frame height. 480 is total, 250 is usable.
+      static const unsigned c_height = 250;
+      //! Configuration parameters.
+      Arguments m_args;
+      //! %Destination log folder.
+      Path m_log_dir;
+      //! Camera handle.
+      HIDS m_cam;
+      //! Thread for image capture.
+      CaptureUeye* m_capture;
+      //! Frame
+      Frame* m_frame;
+      //! Current calibration gain
+      int m_calib_gain;
+      //! Time of last calibration gain change
+      double m_calib_time;
+
+      Task(const std::string& name, Tasks::Context& ctx):
+        Tasks::Task(name, ctx),
+        m_log_dir(ctx.dir_log),
+        m_cam(1),
+        m_capture(NULL),
+        m_frame(NULL),
+        m_calib_gain(0),
+        m_calib_time(0.0)
+      {
+        // Retrieve configuration values.
+        paramActive(Tasks::Parameter::SCOPE_GLOBAL,
+                    Tasks::Parameter::VISIBILITY_USER);
+
+        param("Camera ID", m_args.cam_id)
+        .defaultValue("1")
+        .minimumValue("1")
+        .description("ID of camera to open");
+
+        param("Frames Per Second", m_args.fps)
+        .defaultValue("30")
+        .minimumValue("0")
+        .maximumValue("75")
+        .description("Frames per second");
+
+        param("AOI - X", m_args.aoi.x)
+        .defaultValue("0")
+        .minimumValue("0")
+        .description("X coordinate of upper left corner of AOI");
+
+        param("AOI - Y", m_args.aoi.y)
+        .defaultValue("0")
+        .minimumValue("0")
+        .description("Y coordinate of upper left corner of AOI");
+
+        param("AOI - Width", m_args.aoi.width)
+        .defaultValue("640")
+        .minimumValue("0")
+        .description("Width of AOI");
+
+        param("AOI - Height", m_args.aoi.height)
+        .defaultValue("480")
+        .minimumValue("0")
+        .description("Height of AOI");
+
+        param("Auto Gain", m_args.auto_gain)
+        .defaultValue("false")
+        .description("Enable Auto Gain");
+
+        param("Gain", m_args.gain)
+        .defaultValue("50")
+        .units(Units::Percentage)
+        .minimumValue("0")
+        .maximumValue("100")
+        .description("Sensor Gain");
+
+        param("Exposure", m_args.exposure)
+        .defaultValue("4")
+        .units(Units::Millisecond)
+        .minimumValue("1")
+        .maximumValue("20")
+        .description("Exposure Time");
+
+        param("Log Dir", m_args.log_dir)
+        .defaultValue("")
+        .description("Path to Log Directory");
+
+        param("Calibration Mode", m_args.calib_mode)
+        .defaultValue("false")
+        .description("Enable calibration mode");
+
+        param("Calibration Delta", m_args.calib_delta)
+        .defaultValue("1.0")
+        .units(Units::Second)
+        .minimumValue("0.1")
+        .maximumValue("10.0")
+        .description("Time interval for each gain in calibration mode");
+
+        bind<IMC::LoggingControl>(this);
+      }
+
+      //! Update internal parameters.
+      void
+      onUpdateParameters(void)
+      {
+        m_log_dir = m_args.log_dir;
+        m_cam = m_args.cam_id;
+      }
+
+      //! Acquire resources and buffers.
+      void
+      onResourceAcquisition(void)
+      {
+        m_capture = new CaptureUeye(this, m_args.aoi, m_cam, m_args.fps);
+        m_capture->setGain(m_args.auto_gain, m_args.gain);
+        m_capture->setExposure(m_args.exposure);
+      }
+
+      //! Release allocated resources.
+      void
+      onResourceRelease(void)
+      {
+        if (m_capture != NULL)
+        {
+          delete m_capture;
+          m_capture = NULL;
+        }
+
+        if (m_frame != NULL)
+        {
+          delete m_frame;
+          m_frame = NULL;
+        }
+      }
+
+      //! Initialize resources and start capturing frames.
+      void
+      onResourceInitialization(void)
+      {
+        setEntityState(IMC::EntityState::ESTA_NORMAL, Status::CODE_IDLE);
+      }
+
+      void
+      consume(const IMC::LoggingControl* msg)
+      {
+        if ((msg->getDestination() != getSystemId()))
+          return;
+
+        if (msg->op == IMC::LoggingControl::COP_CURRENT_NAME || msg->op == IMC::LoggingControl::COP_REQUEST_START)
+        {
+          m_log_dir = m_args.log_dir / msg->name;
+          m_log_dir.create();
+        }
+      }
+
+      void
+      onRequestActivation(void)
+      {
+        IMC::LoggingControl log_ctl;
+        log_ctl.op = IMC::LoggingControl::COP_REQUEST_CURRENT_NAME;
+        dispatch(log_ctl);
+        activate();
+      }
+
+      void
+      onActivation(void)
+      {
+        m_log_dir.create();
+        setEntityState(IMC::EntityState::ESTA_NORMAL, Status::CODE_ACTIVE);
+        m_capture->start();
+      }
+
+      void
+      onDeactivation(void)
+      {
+        setEntityState(IMC::EntityState::ESTA_NORMAL, Status::CODE_IDLE);
+        stopCapture();
+      }
+
+      //! Saves the image.
+      void
+      saveImage(Frame* frame)
+      {
+        Path file = m_log_dir / String::str("%0.4f_%07llu_%04d.bmp", frame->timestamp, frame->seqNum, frame->gainFactor);
+
+        const size_t cSize = strlen(file.c_str())+1;
+        std::wstring ws( cSize, L'#' );
+        mbstowcs( &ws[0], file.c_str(), cSize );
+
+        IMAGE_FILE_PARAMS imageFileParams;
+
+        imageFileParams.pwchFileName = const_cast<wchar_t*>(ws.c_str());
+        imageFileParams.pnImageID = &frame->id;
+        imageFileParams.ppcImageMem = &frame->data;
+        // File format: JPEG, PNG or BMP
+        // Using BMP for uncompressed data
+        imageFileParams.nFileType = IS_IMG_BMP;
+
+        int ret = is_ImageFile(m_cam, IS_IMAGE_FILE_CMD_SAVE, (void*)&imageFileParams, sizeof(imageFileParams));
+
+        if (ret != IS_SUCCESS)
+          err("Image write unsuccessful. Error %d", ret);
+      }
+
+      void
+      stopCapture(void)
+      {
+        m_capture->stopCapture();
+
+        bool qhasdata = true;
+        int i = -1;
+
+        debug("Emptying buffer.");
+        while (qhasdata)
+        {
+          m_frame = m_capture->readFrame();
+          if (m_frame == NULL)
+            qhasdata = false;
+          else
+            saveImage(m_frame);
+          i++;
+        }
+
+        debug("%d images in buffer when stopping.", i);
+
+        if (m_capture->isRunning())
+          m_capture->stopAndJoin();
+      }
+
+      void
+      onMain(void)
+      {
+        while (!stopping())
+        {
+          consumeMessages();
+
+          if (!isActive())
+          {
+            Time::Delay::wait(0.5);
+            continue;
+          }
+
+          m_frame = m_capture->readFrame();
+          if (m_frame == NULL)
+            Time::Delay::wait(0.5);
+          else
+          {
+            saveImage(m_frame);
+
+            double now = Time::Clock::get();
+            double delta = now - m_calib_time;
+
+            if (m_args.calib_mode && (delta > m_args.calib_delta))
+            {
+              m_calib_gain++;
+              if (m_calib_gain > 100)
+                m_calib_gain = 0;
+
+              m_capture->setGain(false, m_calib_gain);
+              m_calib_time = now;
+            }
+          }
+        }
+
+        stopCapture();
+      }
+    };
+  }
+}
+
+DUNE_TASK
